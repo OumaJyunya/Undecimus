8f22d64fb7c7214f3c28e7d0b4283a34c82dc08c
Pwn20wnd <pwn20wndstuff@gmail.com>
2019-03-10 14:34:22 +0300

    Optimize get_proc_struct_for_pid as well

--- Undecimus/source/KernelUtilities.c
+++ Undecimus/source/KernelUtilities.c
@@ -101,12 +101,16 @@ mach_port_t fake_host_priv()
 
 uint64_t get_proc_struct_for_pid(pid_t pid)
 {
-    uint64_t proc = ReadKernel64(ReadKernel64(GETOFFSET(kernel_task)) + koffset(KSTRUCT_OFFSET_TASK_BSD_INFO));
-    LOG("proc = " ADDR, proc);
-    if (proc == 0) {
-        LOG("failed to get proc!");
-        return 0;
+    static uint64_t kernproc = 0;
+    if (kernproc == 0) {
+        kernproc = ReadKernel64(ReadKernel64(GETOFFSET(kernel_task)) + koffset(KSTRUCT_OFFSET_TASK_BSD_INFO));
+        LOG("kernproc = " ADDR, kernproc);
+        if (kernproc == 0) {
+            LOG("failed to get kernproc!");
+            return 0;
+        }
     }
+    uint64_t proc = kernproc;
     if (pid == 0) {
         return proc;
     }
